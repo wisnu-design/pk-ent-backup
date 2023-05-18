b54e4f803964c689240ebcd113d166970b6b9642
Wisnu Budi Laksono <wisnubl1995@gmail.com>
2023-05-18 10:28:27 +0700

    api

--- lib/graphql/api.ts
+++ lib/graphql/api.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from "graphql-request";
 
 export const api: GraphQLClient = new GraphQLClient(
-  "https://ap-southeast-2.cdn.hygraph.com/content/clhr1n65i0lfe01uo3sjo1jhz/master"
+  "https://ap-southeast-2.cdn.hygraph.com/content/clhr3p4k00soy01t52pkm53fc/master"
 );
