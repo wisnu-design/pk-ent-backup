f64d84c5507659567ded7c54b526d2d87a8ee3e8
Wisnu Budi Laksono <wisnubl1995@gmail.com>
2023-11-01 11:08:29 +0700

    fix API Limit

--- lib/graphql/api.ts
+++ lib/graphql/api.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from "graphql-request";
 
 export const api: GraphQLClient = new GraphQLClient(
-  "https://ap-southeast-2.cdn.hygraph.com/content/clisd44j41x6c01ui5rptcj4c/master"
+  "https://ap-southeast-2.cdn.hygraph.com/content/clof85qt127nq01t7hc50e1k7/master"
 );
