255278416ebbcbbc155d53a56ad5a29ddc8b71a0
Wisnu Budi Laksono <wisnubl1995@gmail.com>
2024-12-18 10:54:34 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -75,7 +75,7 @@ export default function Home({ concerts, brands, billboard }: any) {
             </button>
             <Link href={"/campaign"}>
               <button className="px-4 py-2 font-medium bg-white  text-black hover:text-white rounded hover:bg-slate-600/30 transition-all ">
-                Join Now!
+                Learn More
               </button>
             </Link>
           </div>
